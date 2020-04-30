a2874bd1f9d206408cff2548a18fbc86777b8a10
John Victor <johnvictorfs@gmail.com>
2020-04-30 20:47:59 -0300

    Add notification and middle-click action when taking screenshots

--- polybar/scripts/updates.sh
+++ polybar/scripts/updates.sh
@@ -30,7 +30,7 @@ while true; do
         elif (( UPDATES > 1 )); then
             echo " $UPDATES"
         else
-            echo $BAR_ICON
+            echo ""
         fi
         sleep 10
         get_total_updates
@@ -39,7 +39,7 @@ while true; do
     # when no updates are available, use a longer loop, this saves on CPU
     # and network uptime, only checking once every 30 min for new updates
     while (( UPDATES == 0 )); do
-        echo $BAR_ICON
+        echo ""
         sleep 1800
         get_total_updates
     done
