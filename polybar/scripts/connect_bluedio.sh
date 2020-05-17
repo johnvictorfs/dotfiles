84546f22e0b127d568f3fb80846ff837d6d44c5b
John Victor <johnvictorfs@gmail.com>
2020-05-17 19:06:53 -0300

    Update script to connect to bluetooth device

--- polybar/scripts/connect_bluedio.sh
+++ polybar/scripts/connect_bluedio.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+ICON="bluetooth"
 
-bluetoothctl connect 16:08:26:18:19:57
+# Set address to first argument if it exists
+[ "$*" == "" ] && ADDRESS="16:08:26:18:19:57" || ADDRESS=$1
+
+CONNECT=$(bluetoothctl connect $ADDRESS)
+
+# Get last two words of message
+MESSAGE=$(echo $CONNECT | tr ' ' '\n' | tail -2 | xargs -n5)
+
+success() {
+  DEVICE_NAME=$(bluetoothctl paired-devices | awk 'NF{print $NF; exit}')
+  notify-send -i "$ICON" "Bluetooth" "Connected successfully to $DEVICE_NAME"
+}
+
+fail() {
+  notify-send -i "$ICON" "Bluetooth" "Could not connect to device: $ADDRESS"
+}
+
+[ "$MESSAGE" = "Connection successful" ] && success || fail
 
