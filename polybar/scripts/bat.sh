117ed0db08f6951e7dab221c0ae9a4bd766cf97b
John Victor <johnvictorfs@gmail.com>
2020-05-21 23:54:51 -0300

    Remove unused polybar config

--- polybar/scripts/bat.sh
+++ polybar/scripts/bat.sh
@@ -2,14 +2,13 @@
 STATUS=$(cat /sys/class/power_supply/BAT0/status)
 CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
 
-[ $CAPACITY -lt "20" ] && ICON="" && UNDERLINE="#e53935"
 [ $CAPACITY -ge "20" ] && ICON="" && UNDERLINE="#ffb300"
 [ $CAPACITY -ge "40" ] && ICON="" && UNDERLINE=""
 [ $CAPACITY -ge "60" ] && ICON="" && UNDERLINE=""
 [ $CAPACITY -ge "80" ] && ICON="" && UNDERLINE=""
 
 # Green underline when Full/Charging
-[ $STATUS != "Discharging" ] && UNDERLINE="#43a047"
+[ $STATUS != "Discharging" ] && ICON="" && UNDERLINE="#43a047"
 
 echo "%{u$UNDERLINE}$ICON $CAPACITY%"
 
