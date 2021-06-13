0ad7c8e58a5cf4712e225febd37d67112a96463e
John <johnvfs@protonmail.com>
2021-06-12 21:01:09 -0300

    Add error handling to duolingo polybar script

--- polybar/scripts/duolingo_streak.py
+++ polybar/scripts/duolingo_streak.py
@@ -4,11 +4,15 @@ import sys
 import os
 import duolingo
 
-lingo = duolingo.Duolingo(os.environ['DUOLINGO_USERNAME'], os.environ['DUOLINGO_PASSWORD'])
+try:
+    lingo = duolingo.Duolingo(os.environ['DUOLINGO_USERNAME'], os.environ['DUOLINGO_PASSWORD'])
 
-streak = lingo.get_streak_info()
+    streak = lingo.get_streak_info()
 
-if streak['streak_extended_today']:
-    print(f' {streak["site_streak"]}')
-else:
-    print('%{F#fc5549} ' + str(streak["site_streak"]))
+    if streak['streak_extended_today']:
+        print(f' {streak["site_streak"]}')
+    else:
+        print('%{F#fc5549} ' + str(streak["site_streak"]))
+except Exception:
+    print('%{F#fc5549} ')
+    pass
