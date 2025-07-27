53d195dcd279a885fc71bd3decc3686131b562be
johnvictorfs <37747572+johnvictorfs@users.noreply.github.com>
2025-07-27 19:39:27 -0300

    update polybar config

--- polybar/scripts/bat.sh
+++ polybar/scripts/bat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 BAT=$(/usr/bin/ls /sys/class/power_supply | grep BAT | head -n 1)
-BAT_PATH=/sys/class/power_supply/$BAT
+BAT_PATH="/sys/class/power_supply/$BAT"
 
-[ -d $BAT_PATH ] || exit 1
+[ -d "$BAT_PATH" ] || exit 1
 
-STATUS=$(cat $BAT_PATH/status)
-CAPACITY=$(cat $BAT_PATH/capacity)
+STATUS=$(cat "$BAT_PATH/status")
+CAPACITY=$(cat "$BAT_PATH/capacity")
 
 RED="#e53935"
 ORANGE="#ffb300"
@@ -15,14 +15,13 @@ GREEN="#21d121"
 
 CLEAR="%{U-}"
 
-[ $CAPACITY -lt "20" ] && ICON="" && UNDERLINE="$RED"
-[ $CAPACITY -ge "20" ] && ICON="" && UNDERLINE="$ORANGE"
-[ $CAPACITY -ge "40" ] && ICON="" && UNDERLINE=""
-[ $CAPACITY -ge "60" ] && ICON="" && UNDERLINE=""
-[ $CAPACITY -ge "80" ] && ICON="" && UNDERLINE=""
+[ "$CAPACITY" -lt 20 ] && ICON="󰁺" && UNDERLINE="$RED"
+[ "$CAPACITY" -ge 20 ] && ICON="󰁼" && UNDERLINE="$ORANGE"
+[ "$CAPACITY" -ge 40 ] && ICON="󰁽" && UNDERLINE=""
+[ "$CAPACITY" -ge 60 ] && ICON="󰁿" && UNDERLINE=""
+[ "$CAPACITY" -ge 80 ] && ICON="󰂁" && UNDERLINE=""
 
-# Full/Charging
-[ $STATUS != "Discharging" ] && ICON=""
+[ "$STATUS" != "Discharging" ] && ICON="󰂄"
 
 echo "%{u$UNDERLINE}%{T5}$ICON%{T-} $CAPACITY%$CLEAR"
 
--- polybar/scripts/microphone.sh
+++ polybar/scripts/microphone.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 GRAY="%{F#757575}"
+MIC_SOURCE="112935"
 
 get_status_icon() {
   #`pactl list | sed -n '/^Source/,/^$/p' | grep Mute | grep yes > /dev/null`
 
   #RESULT=$(amixer get Capture | tail -2 | grep -c '\[on\]')
-  RESULT=$(pamixer --get-mute --source 55)
+  RESULT=$(pamixer --get-mute --source $MIC_SOURCE)
 
   if [ $RESULT = true ]; then
-    echo "$GRAY"
+    echo "$GRAY󰍭"
   else
 
     #VOLUME=$(amixer get Capture | egrep -o '([0-9]{2}%)' | tail -n 1)
-    VOLUME=$(pamixer --get-volume --source 55)
-    echo " $VOLUME%"
+    VOLUME=$(pamixer --get-volume --source $MIC_SOURCE)
+    echo "󰍬 $VOLUME%"
   fi
 }
 
